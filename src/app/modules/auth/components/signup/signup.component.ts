83753277d8147e106d93c398099c5ae38121fdc4
Shahed Tareq <shahed@logicteca.com>
2023-09-02 06:05:56 +0300

    remove unused style and add loading on signup button

--- src/app/modules/auth/components/signup/signup.component.ts
+++ src/app/modules/auth/components/signup/signup.component.ts
@@ -4,10 +4,7 @@ import { Router } from '@angular/router';
 import { InputPatterns } from 'src/app/modules/shared/utils/validation-pattern';
 import { AuthService } from '../../services/auth.service';
 import { LoaderComponent } from 'src/app/modules/shared/components/loader/loader.component';
-interface User {
-  name: string;
-  code: string;
-}
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -16,24 +13,23 @@ interface User {
 export class SignupComponent implements OnInit{
 public submitted:boolean = false;
 userImage:any;
-users:User[]=[];
 signupForm!:FormGroup;
-
+public loading:boolean = false;
 imageForm!:FormGroup;
+public hasImage:boolean = false;
+public isExist:boolean = false;
+public plantImage:string = ''
 
 constructor(private router:Router , private fb:FormBuilder , private authService: AuthService){
   
 }
 ngOnInit(): void {
 this.signupFormInitialization()
-  this.users = [
-   { name: 'Farmer', code: '1' },
-   { name: 'Customer', code: '2' }
-  ]
  }
 
 public signup(){
   this.submitted = true;
+  this.loading = true;
   if(this.signupForm.valid){
   const userData = this.signupForm.value;
     this.submitted = false;
@@ -46,18 +42,20 @@ public signup(){
     formData.append('UserName', userData?.userName);
     formData.append('ImageFile', this.userImage);
     this.authService.signUp(formData).subscribe((result:any)=>{
-    localStorage.setItem('email' ,  userData?.email)
-      this.router.navigate(['/auth/confirm'])
-    })
-   
-  } else{
-    
-  }
+if(result.isSuccess){
+  localStorage.setItem('email' ,  userData?.email)
+  this.router.navigate(['/auth/confirm'])
+  this.loading = false;
+} else{
+  this.loading = false;
+}
+}, (error) => {
+  console.error('An error occurred:', error);
+  this.loading = false;
+})}
 }
 
-public hasImage:boolean = false;
-public isExist:boolean = false;
-public plantImage:string = ''
+
 
 public selectPhoto(event:any){
 this.hasImage = true;
